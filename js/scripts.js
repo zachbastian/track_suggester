b4dc89d967c7527e25ba88ba0b67ba9d4044177a
Zach Bastian <azbastian@gmail.com>
2017-03-24 14:15:25 -0700

    it will add h4 headers, new font family, more branching logic.

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,19 @@ $(document).ready(function() {
     var quest5 = $("#q5").val().toLowerCase();
     var nameInput = $("input#name").val();
 
-    if (quest1 === "yes" && quest2 === "no" && quest3 === "no" && quest4 === "no" && quest5 === "no") {
+    if (quest1 === "yes" && quest2 === "no" && quest3 === "no" && quest4 === "no" && quest5 === "no" || quest1 === "maybe" && quest3 === "maybe") {
       $(".php, .java, .css, .csharp, .notsure").hide();
       $(".ruby").fadeIn();
-    } else if (quest1 === "no" && quest2 === "yes" && quest3 === "no" && quest4 === "no" && quest5 === "no") {
+    } else if (quest1 === "no" && quest2 === "yes" && quest3 === "no" && quest4 === "no" && quest5 === "no" || quest2 === "maybe" && quest5 === "maybe") {
       $(".ruby, .java, .css, .csharp, .notsure").hide();
       $(".php").fadeIn();
-    } else if (quest1 === "no" && quest2 === "no" && quest3 === "yes" && quest4 === "no" && quest5 === "no") {
+    } else if (quest1 === "no" && quest2 === "no" && quest3 === "yes" && quest4 === "no" && quest5 === "no" || quest3 === "maybe" && quest4 === "maybe") {
       $(".php, .ruby, .css, .csharp, .notsure").hide();
       $(".java").fadeIn();
-    } else if (quest1 === "no" && quest2 === "no" && quest3 === "no" && quest4 === "yes" && quest5 === "no") {
+    } else if (quest1 === "no" && quest2 === "no" && quest3 === "no" && quest4 === "yes" && quest5 === "no" || quest4 === "maybe" && quest1 === "maybe") {
       $(".php, .java, .ruby, .csharp, .notsure").hide();
       $(".css").fadeIn();
-    } else if (quest1 === "no" && quest2 === "no" && quest3 === "no" && quest4 === "no" && quest5 === "yes") {
+    } else if (quest1 === "no" && quest2 === "no" && quest3 === "no" && quest4 === "no" && quest5 === "yes" || quest5 === "maybe" && quest2 === "maybe") {
       $(".php, .java, .css, .ruby, .notsure").hide();
       $(".csharp").fadeIn();
     } else {
