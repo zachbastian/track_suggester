fa5ba6841a6e4e112f69f5815c27f0652cdeb24b
Zach Bastian <azbastian@gmail.com>
2017-03-24 09:21:41 -0700

    it will add simple, functional logic to branching, jumbotron element, background colors, and new font families.

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,31 @@ $(document).ready(function() {
 
   $("form").submit(function(event) {
   event.preventDefault();
-    var question1 = $("#q1").val();
-    var question2 = $("#q2").val();
-    var question3 = $("#q3").val();
-    var question4 = $("#q4").val();
-    var question5 = $("#q5").val();
+    var quest1 = $("#q1").val().toLowerCase();
+    var quest2 = $("#q2").val().toLowerCase();
+    var quest3 = $("#q3").val().toLowerCase();
+    var quest4 = $("#q4").val().toLowerCase();
+    var quest5 = $("#q5").val().toLowerCase();
 
+    if (quest1 === "yes" && quest2 === "no" && quest3 === "no" && quest4 === "no" && quest5 === "no") {
+      $(".php, .java, .css, .csharp, .notsure").hide();
+      $(".ruby").show();
+    } else if (quest1 === "no" && quest2 === "yes" && quest3 === "no" && quest4 === "no" && quest5 === "no") {
+      $(".ruby, .java, .css, .csharp, .notsure").hide();
+      $(".php").show();
+    } else if (quest1 === "no" && quest2 === "no" && quest3 === "yes" && quest4 === "no" && quest5 === "no") {
+      $(".php, .ruby, .css, .csharp, .notsure").hide();
+      $(".java").show();
+    } else if (quest1 === "no" && quest2 === "no" && quest3 === "no" && quest4 === "yes" && quest5 === "no") {
+      $(".php, .java, .ruby, .csharp, .notsure").hide();
+      $(".css").show();
+    } else if (quest1 === "no" && quest2 === "no" && quest3 === "no" && quest4 === "no" && quest5 === "yes") {
+      $(".php, .java, .css, .ruby, .notsure").hide();
+      $(".csharp").show();
+    } else {
+      $(".php, .java, .css, .csharp, .ruby").hide();
+      $(".notsure").show();
+    }
 
-if (question1 === "yes") {
-  $(".ruby").show();
-} else if (question2 === "yes") {
-  $(".php").show();
-} else if (question3 === "yes") {
-  $(".java").show();
-} else if (question4 === "yes") {
-  $(".css").show();
-} else if (question5 === "yes") {
-  $(".csharp").show();
-} else {
-  $(".notsure").show();
-}
-
+  });
 });
