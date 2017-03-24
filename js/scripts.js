c9c76e54b7870fa98aefae0b84e78ca0b6afd791
Zach Bastian <azbastian@gmail.com>
2017-03-24 08:41:50 -0700

    it will add starting logic to JavaScript branching.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-$(function() {
+$(document).ready(function() {
 
   $("form").submit(function(event) {
   event.preventDefault();
-var question1 = $("#q1").val();
-var question2 = $("#q2").val();
-var question3 = $("#q3").val();
-var question4 = $("#q4").val();
-var question5 = $("#q5").val();
+    var question1 = $("#q1").val();
+    var question2 = $("#q2").val();
+    var question3 = $("#q3").val();
+    var question4 = $("#q4").val();
+    var question5 = $("#q5").val();
 
 
-if (logic) {
+if (question1 === "yes") {
   $(".ruby").show();
-} else if (logic) {
+} else if (question2 === "yes") {
   $(".php").show();
-} else if (logic) {
+} else if (question3 === "yes") {
   $(".java").show();
-} else if (logic) {
+} else if (question4 === "yes") {
   $(".css").show();
-} else if (logic) {
+} else if (question5 === "yes") {
   $(".csharp").show();
 } else {
-  $(".error").show();
+  $(".notsure").show();
 }
 
 });
